7ea3b6eabfee78aa1960172b5c868af579829c91
Fagner <meurerfagner@hotmail.com>
2018-10-20 13:13:20 -0300

    Cridao Hud para controle de Vida,Placar e respawn
    Criado Scriptibal Object para informações do player no hud, Criado objetos do tipo PlayerHudInformation para os dois player, criado prephab para Hud e CellLife.
    Criado script com a logica de funcionante dos scores e vidas de cada Player

---
+++ Assets/_Game/Scripts/PlayerHud.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHud : MonoBehaviour
+{
+
+    public PlayerHudInformation PlayerInfo;
+    public Image[] LifeCell = new Image[5];
+    public Image[] BallSlots = new Image[4];
+    public Image PlayerColor;
+
+    int score;
+    int damageRecived;
+    float timeRespawn;
+    bool isDeath;
+    // Use this for initialization
+    void Start()
+    {
+        foreach (var ball in FindObjectsOfType<Ball>())
+        {
+            if (ball.PlayerNumber == PlayerInfo.PlayerNumber)
+                ball.OnScore.AddListener(Score);
+        }
+        foreach (var player in FindObjectsOfType<PlayerLife>())
+        {
+            player.OnDamage.AddListener(Damage);
+            player.OnRespawn.AddListener(Respawn);
+            player.OnDeath.AddListener(Death);
+        }
+        PlayerColor.color = PlayerInfo.PlayerColor;
+        for (int i = 0; i < BallSlots.Length; i++)
+        {
+            BallSlots[i].sprite = PlayerInfo.BallInactive;
+        }
+        FullLifeBar();
+    }
+
+    private void FullLifeBar()
+    {
+        for (int i = 0; i < LifeCell.Length; i++)
+        {
+            LifeCell[i].color = PlayerInfo.LifeCellColor;
+            LifeCell[i].fillAmount = 1;
+        }
+        damageRecived = 0;
+    }
+    private void ClearLifeBar()
+    {
+        for (int i = 0; i < LifeCell.Length; i++)
+        {
+            LifeCell[i].fillAmount = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Score(int playerNumber)
+    {
+        if (playerNumber == PlayerInfo.PlayerNumber)
+        {
+            BallSlots[score].sprite = PlayerInfo.BallActive;
+            score++;
+        }
+    }
+    void Damage(PlayerLife player)
+    {
+        if (player.playerNumber == PlayerInfo.PlayerNumber)
+        {
+            LifeCell[damageRecived].fillAmount = 0;
+            damageRecived++;
+        }
+    }
+    void Respawn(PlayerLife player)
+    {
+        if (player.playerNumber == PlayerInfo.PlayerNumber)
+        {
+            FullLifeBar();
+        }
+
+    }
+
+    IEnumerator ChargeLifeBar(float timeRespawn)
+    {
+        //TEMPO PARA RECARR
+        var intervalo = timeRespawn / LifeCell.Length;
+        for (int i = 0; i < LifeCell.Length; i++)
+        {
+            yield return new WaitForSeconds(intervalo);
+            LifeCell[i].fillAmount = 1;
+        }
+    }
+    void Death(PlayerLife player)
+    {
+        if (player.playerNumber == PlayerInfo.PlayerNumber)
+        {
+            ClearLifeBar();
+            StartCoroutine("ChargeLifeBar", player.TotalTimeRespawn);
+        }
+    }
+
+
+}
---
+++ Assets/_Game/Scripts/ScriptableObjects/PlayerHudInformation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName ="New PlayerInfo", menuName ="PalyerInfo")]
+public class PlayerHudInformation : ScriptableObject {
+
+    public int PlayerNumber;
+    public Sprite BallInactive;
+    public Sprite BallActive;
+    public Color PlayerColor;
+    public Color LifeCellColor;
+
+}
