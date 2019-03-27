29890a55ab725a89c7e6bfb5422dd33f47fafbf9
Henrique Jardim <henriquefjardim@gmail.com>
2019-03-27 11:22:28 -0300

    Novas mesas

---
+++ Assets/BumperHit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumperHit : MonoBehaviour {
+    [SerializeField]
+    private float    hitForce = 0f;
+
+    private void OnCollisionEnter(Collision collision) {
+        ApplyImpact(collision);
+    }
+
+    void ApplyImpact(Collision collision) {
+
+        if (!collision.gameObject.CompareTag("Ball")) return;
+
+        print("Bumper hit");
+        var rb = collision.gameObject.GetComponent<Rigidbody>();
+
+        rb.AddForce(((-1f) * collision.contacts[0].normal) * hitForce, ForceMode.Impulse);
+    }
+}
---
+++ Assets/ObstacleManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleManager : MonoBehaviour {
+
+    public GameObject[] obstacles;
+
+	// Use this for initialization
+	void Start () {
+        var r = Random.Range(0, obstacles.Length);
+
+        obstacles[r].SetActive(true);
+
+	}
+	 
+}
